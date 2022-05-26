93282878e4d9680c57d36d441d5ee479c0d8af1a
Claudia <claudia.araya.ro@gmail.com>
2022-05-26 00:02:51 -0400

    subiendo desafío guiado productora de eventos

---
+++ app/models/concert.rb
@@ -0,0 +1,7 @@
+class Concert < ApplicationRecord
+    belongs_to :group
+
+    def concert_name_group 
+        self.group.name
+    end
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :groups
+  resources :concerts
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "concerts#index"
+  # root "articles#index"
+end
